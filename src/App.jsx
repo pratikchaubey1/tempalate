962aab11080053022f1034f702249c1adaae55b5
Pratik Chaubey <pratikup98@gmail.com>
2025-08-04 21:40:30 +0530

    api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
 
 function App() {
+  const url = "https://fakestoreapi.com/products/";
+  const [Card, setCard] = useState([]);
+  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log("Server ka data: ", data);
+        setProducts(data);
+      } catch (error) {
+        console.log("Error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+ 
+
   return (
-    <div>App</div>
-  )
+    <div>
+      <Navbar></Navbar>
+      <Card></Card>
+    </div>
+  );
 }
 
-export default App
+export default App;
